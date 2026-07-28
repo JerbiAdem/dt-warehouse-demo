adb43980edb28cbaeefbeacb005b53a5c8cae43a
AndreaCecca <andrea.ceccarelli@unifi.it>
2026-07-28 10:49:25 +0200

    fix: indentation error in warehouse_manager_node

--- packages/warehouse/src/warehouse_manager_node.py
+++ packages/warehouse/src/warehouse_manager_node.py
@@ -28,8 +28,8 @@ class WarehouseManagerNode:
         self.node_name = rospy.get_name()
         self.veh = rospy.get_param('~veh', 'mybot')
 
-	rospy.loginfo(f'[{self.node_name}] Vehicle name: {self.veh}')
-	rospy.loginfo(f'[{self.node_name}] Subscribing to: /{self.veh}/apriltag_detector_node/detections')
+        rospy.loginfo(f'[{self.node_name}] Vehicle name: {self.veh}')
+        rospy.loginfo(f'[{self.node_name}] Subscribing to: /{self.veh}/apriltag_detector_node/detections')
 
         # FSM
         self.state = STATE_NAVIGATING
